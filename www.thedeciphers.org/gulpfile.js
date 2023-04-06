b018d7a6424e422107335fb72d94148462ac6f53
Ishwari Katariya <ishwarikatariya@MacBook-Pro.local>
2023-04-06 19:05:35 +0100

    initial commit

---
+++ www.thedeciphers.org/gulpfile.js
@@ -0,0 +1,8 @@
+﻿const gulp = require('gulp');
+const htmlmin = require('gulp-htmlmin');
+
+gulp.task('minify-html', function () {
+    return gulp.src('*.html')
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest('dist'));
+});
